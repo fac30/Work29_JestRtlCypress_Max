6c66ebe6f2c5023fd2e49a983a3b7652dbc555d2
maxitect <maxime.downe@gmail.com>
2024-11-29 15:58:35 +0000

    test: exercise 1 cypress end-to-end testing

--- cypress/e2e/exercise-one/exercise-one.cy.js
+++ cypress/e2e/exercise-one/exercise-one.cy.js
@@ -6,16 +6,25 @@ describe("Exercise One", () => {
 
   it("should display the correct title", () => {
     // Fix this failing test
-    cy.get(".title").should("contain.text", "Exercise Two");
+    cy.get(".title").should("contain.text", "Exercise One");
+  });
+
+  it("should display the correct description text", () => {
     cy.get(".text").should(
       "contain.text",
       "This is a simple UI exercise page to test navigation and button presence."
     );
   });
 
-  it("should display the correct description text", () => {});
+  it("should display a button and trigger alert on click", () => {
+    cy.get("button").click();
+    cy.on("window:alert", (text) => {
+      expect(text).to.eq("Button clicked!");
+    });
+  });
 
-  it("should display a button and trigger alert on click", () => {});
-
-  it("should have a working link to the Home page", () => {});
+  it("should have a working link to the Home page", () => {
+    cy.get(".link").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
 });
