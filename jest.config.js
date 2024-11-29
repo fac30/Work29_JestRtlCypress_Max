28522aa9cd1e1841c2d0abda24100db0547e1d21
maxitect <maxime.downe@gmail.com>
2024-11-29 12:41:41 +0000

    build: initial installs and configurations

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,7 @@ const createJestConfig = nextJest({ dir: "./" });
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jest-environment-jsdom",
-  testMatch: [
-    "<rootDir>/src/**/?(*.)+(test).[jt]s?(x)", // Match test files in src directory, including subdirectories
-    "!**/*.solution.test.[jt]s?(x)",
-  ],
+  //testMatch: ["<rootDir>/src/*.test.js", "<rootDir>/src/*.test.jsx"],
 };
 
 module.exports = createJestConfig(customJestConfig);
--- src/test-utils/basicMath/basicMath.test.js
+++ src/test-utils/basicMath/basicMath.test.js
@@ -1,4 +1,4 @@
-import { add } from "./basicMath";
+import { add, subtract, multiply, divide, square } from "./basicMath";
 // Basic maths functions
 
 // 1a - add
@@ -16,53 +16,101 @@ it("should return the sum of two positive numbers", () => {
 
 it("should return the sum of two negative numbers", () => {
   // Arrange
+  const a = -2;
+  const b = -4;
+
   // Act
+  const result = add(a, b);
+
   // Assert
+  expect(result).toEqual(-6);
 });
 
 // 1b - subtract
 it("should return the difference of two numbers", () => {
   // Arrange
+  const a = 3;
+  const b = 2;
+
   // Act
+  const result = subtract(a, b);
+
   // Assert
+  expect(result).toEqual(1);
 });
 
 // 1c - multiply
 it("should return the product of two positive numbers", () => {
   // Arrange
+  const a = 3;
+  const b = 2;
+
   // Act
+  const result = multiply(a, b);
+
   // Assert
+  expect(result).toEqual(6);
 });
 
 it("should return a negative product when one factor is negative", () => {
   // Arrange
+  const a = 3;
+  const b = -2;
+
   // Act
+  const result = multiply(a, b);
+
   // Assert
+  expect(result).toEqual(-6);
 });
 
 // Stretch: Basic maths functions with errors
 // 2a - divide with an error
 it("should return the quotient of two positive numbers", () => {
   // Arrange
+  const a = 4;
+  const b = 2;
+
   // Act
+  const result = divide(a, b);
+
   // Assert
+  expect(result).toEqual(2);
 });
 
 it("should throw an error when dividing by zero", () => {
   // Arrange
+  const a = 3;
+  const b = 0;
+
   // Act
+  //const result = divide(a, b);
+
   // Assert
+  expect(() => divide(a, b)).toThrow();
 });
 
 // 2b - sqrt with an error
 it("should return the square of a positive number", () => {
   // Arrange
+  const a = 3;
+  const b = 2;
+
   // Act
+  const result = square(a, b);
+
   // Assert
+  expect(result).toEqual(9);
 });
 
 it("should return the square of a negative number", () => {
   // Arrange
+  const a = -3;
+  const b = 2;
+
   // Act
+  const result = square(a, b);
+
   // Assert
+  expect(result).toEqual(9);
 });
